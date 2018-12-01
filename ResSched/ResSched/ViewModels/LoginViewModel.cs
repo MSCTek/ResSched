2d6862b3fce1967bf16b489e59a7f3c74feb7b73
robin@msctek.com <robin@msctek.com>
2018-12-01 17:58:42 -0600

    Added empty view models for login and myReservations

---
+++ ResSched/ResSched/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResSched.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public LoginViewModel()
+        {
+
+        }
+
+    }
+}
---
+++ ResSched/ResSched/ViewModels/MyReservationsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResSched.ViewModels
+{
+    public class MyReservationsViewModel : BaseViewModel
+    {
+        public MyReservationsViewModel()
+        {
+            Title = "My Reservations";
+        }
+    }
+}

ce6d36a259381467883af505133c3eb40d433033
robin@msctek.com <robin@msctek.com>
2018-12-04 00:32:16 -0600

    Added MVVMLight, obj models and a bit of sample data

---
+++ ResSched/ResSched/Models/Obj/ResourceSchedule.cs
@@ -0,0 +1,63 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResSched.Models.Obj
+{
+    public class ResourceSchedule : ObservableObject
+    {
+        private DateTime _reservationDateTime;
+        private string _reservationNotes;
+        private Guid _reservedByUserId;
+        private string _reservedForUser;
+        private DateTime _reservedOnDateTime;
+        private Guid _resourceId;
+        private Guid _resourceScheduleId;
+
+        public ResourceSchedule()
+        {
+        }
+
+        public DateTime ReservationDateTime
+        {
+            get { return _reservationDateTime; }
+            set { Set(nameof(ReservationDateTime), ref _reservationDateTime, value); }
+        }
+
+        public string ReservationNotes
+        {
+            get { return _reservationNotes; }
+            set { Set(nameof(ReservationNotes), ref _reservationNotes, value); }
+        }
+
+        public Guid ReservedByUserId
+        {
+            get { return _reservedByUserId; }
+            set { Set(nameof(ReservedByUserId), ref _reservedByUserId, value); }
+        }
+
+        public string ReservedForUser
+        {
+            get { return _reservedForUser; }
+            set { Set(nameof(ReservedForUser), ref _reservedForUser, value); }
+        }
+
+        public DateTime ReservedOnDateTime
+        {
+            get { return _reservedOnDateTime; }
+            set { Set(nameof(ReservedOnDateTime), ref _reservedOnDateTime, value); }
+        }
+        public Guid ResourceId
+        {
+            get { return _resourceId; }
+            set { Set(nameof(ResourceId), ref _resourceId, value); }
+        }
+
+        public Guid ResourceScheduleId
+        {
+            get { return _resourceScheduleId; }
+            set { Set(nameof(ResourceScheduleId), ref _resourceScheduleId, value); }
+        }
+    }
+}

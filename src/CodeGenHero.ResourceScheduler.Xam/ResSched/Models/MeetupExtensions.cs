7b55d3fca5050ce72da331a2ff462c9e41ad718c
Paul <Paul@msctek.com>
2019-01-22 18:12:39 -0600

    Fix for issue #10 - Meetup feed is not in chronological order.  Move ErrorDescription property to base viewmodel class.

---
+++ src/CodeGenHero.ResourceScheduler.Xam/ResSched/Models/MeetupExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+
+namespace ResSched.Models.MeetupEvents
+{
+    public static class MeetupExtensions
+    {
+        public static ObservableCollection<Result> SortByTime(this IEnumerable<Result> source, ListSortDirection sortDirection)
+        {
+            var retVal = new ObservableCollection<Result>();
+
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            if (sortDirection == ListSortDirection.Ascending)
+            {
+                retVal = new ObservableCollection<Result>(source.OrderBy(x => x.time));
+            }
+            else
+            {
+                retVal = new ObservableCollection<Result>(source.OrderByDescending(x => x.time));
+            }
+
+            return retVal;
+        }
+    }
+}

962c213434580a244efc11a132231fbcb8fa6bdb
AzureAD\RobinSchroeder <robin@msctek.com>
2019-01-24 15:22:52 -0600

    Added back button supress on the login page,  fixed UI on modReservation page

--- src/CodeGenHero.ResourceScheduler.Xam/ResSched/Views/Login.xaml.cs
+++ src/CodeGenHero.ResourceScheduler.Xam/ResSched/Views/Login.xaml.cs
@@ -24,5 +24,15 @@ namespace ResSched.Views
                 await vm.InitVM();
             }
         }
+
+        protected override bool OnBackButtonPressed()
+        {
+            // If you want to continue going back
+            //base.OnBackButtonPressed();
+            //return false;
+
+            // If you want to stop the back button
+            return true;
+        }
     }
 }
